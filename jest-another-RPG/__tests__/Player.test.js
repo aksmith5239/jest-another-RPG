48e204913fab751941e1518f0a64064c1f74f2ff
aksmith5239 <aklobby@gmail.com>
2020-09-29 17:21:29 -0600

    updated player object

--- jest-another-RPG/__tests__/Player.test.js
+++ jest-another-RPG/__tests__/Player.test.js
@@ -15,4 +15,23 @@ test('creates a player object', () => {
     );
     
 
-})
+});
+
+test('gets a players stats as an object', () => {
+    const player = new Player('Allison');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Allison');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
